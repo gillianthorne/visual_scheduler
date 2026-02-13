f7ca26922eb92db1469c69fc30b0908dc7ea389b
ducky <gillianthorne1353@gmail.com>
2026-02-13 17:26:07 -0500

    changed data types to work better with how i want this project set up

--- lib/features/tasks/data/task_model.dart
+++ lib/features/tasks/data/task_model.dart
@@ -11,10 +11,10 @@ class Task extends HiveObject {
   final String title;
 
   @HiveField(2)
-  final DateTime startTime;
+  final Duration startOffset;
 
   @HiveField(3)
-  final DateTime endTime;
+  final Duration duration;
 
   @HiveField(4)
   final String? categoryId;
@@ -31,44 +31,50 @@ class Task extends HiveObject {
   @HiveField(8)
   final String? notes;
 
+  @HiveField(9)
+  final DateTime date;
+
   Task({
     required this.id,
     required this.title,
-    required this.startTime,
-    required this.endTime,
+    required this.startOffset,
+    required this.duration,
     this.categoryId,
     this.templateId,
     this.allowOverlap = true,
     this.isReminder = false,
-    this.notes
+    this.notes,
+    required this.date
   });
 
   Task copyWith({
     String? id,
     String? title,
-    DateTime? startTime,
-    DateTime? endTime,
+    Duration? startOffset,
+    Duration? duration,
     String? categoryId,
     String? templateId,
     bool? allowOverlap,
     bool? isReminder,
     String? notes,
+    DateTime? date
   }) {
     return Task(
       id: id ?? this.id,
       title: title ?? this.title,
-      startTime: startTime ?? this.startTime,
-      endTime: endTime ?? this.endTime,
+      startOffset: startOffset ?? this.startOffset,
+      duration: duration ?? this.duration,
       categoryId: categoryId ?? this.categoryId,
       templateId: templateId ?? this.templateId,
       allowOverlap: allowOverlap ?? this.allowOverlap,
       isReminder: isReminder ?? this.isReminder,
       notes: notes ?? this.notes,
+      date: date ?? this.date
     );
   }
 
   @override
   String toString() {
-    return "Task(title: $title, start: $startTime, end: $endTime)";
+    return "Task(title: $title, start: $startOffset, duration: $duration)";
   }
 }
--- lib/features/tasks/data/task_model.g.dart
+++ lib/features/tasks/data/task_model.g.dart
@@ -19,28 +19,29 @@ class TaskAdapter extends TypeAdapter<Task> {
     return Task(
       id: fields[0] as String,
       title: fields[1] as String,
-      startTime: fields[2] as DateTime,
-      endTime: fields[3] as DateTime,
+      startOffset: fields[2] as Duration,
+      duration: fields[3] as Duration,
       categoryId: fields[4] as String?,
       templateId: fields[5] as String?,
       allowOverlap: fields[6] as bool,
       isReminder: fields[7] as bool,
       notes: fields[8] as String?,
+      date: fields[9] as DateTime,
     );
   }
 
   @override
   void write(BinaryWriter writer, Task obj) {
     writer
-      ..writeByte(9)
+      ..writeByte(10)
       ..writeByte(0)
       ..write(obj.id)
       ..writeByte(1)
       ..write(obj.title)
       ..writeByte(2)
-      ..write(obj.startTime)
+      ..write(obj.startOffset)
       ..writeByte(3)
-      ..write(obj.endTime)
+      ..write(obj.duration)
       ..writeByte(4)
       ..write(obj.categoryId)
       ..writeByte(5)
@@ -50,7 +51,9 @@ class TaskAdapter extends TypeAdapter<Task> {
       ..writeByte(7)
       ..write(obj.isReminder)
       ..writeByte(8)
-      ..write(obj.notes);
+      ..write(obj.notes)
+      ..writeByte(9)
+      ..write(obj.date);
   }
 
   @override
