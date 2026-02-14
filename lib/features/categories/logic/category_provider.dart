1d6df0ca0bad0b0ecad973b8a31cd98ba590c633
ducky <gillianthorne1353@gmail.com>
2026-02-13 19:48:33 -0500

    broke it

--- lib/features/categories/logic/category_provider.dart
+++ lib/features/categories/logic/category_provider.dart
@@ -1,62 +1,50 @@
-import "package:flutter/foundation.dart" hide Category;
+import 'package:flutter/foundation.dart' hide Category;
 import 'package:hive/hive.dart';
+
 import '../data/category_model.dart';
-import '../data/category_repository.dart';
 
 class CategoryProvider extends ChangeNotifier {
-  final CategoryRepository _repository;
-  List<Category> _categories = [];
   final Box<Category> _box = Hive.box<Category>('categories');
+
+  List<Category> _categories = [];
 
   List<Category> get categories => _categories;
 
-  CategoryProvider(this._repository) {
+  CategoryProvider() {
     _loadCategories();
   }
 
   Future<void> _loadCategories() async {
-    _categories = await _repository.getAll();
+    _categories = _box.values.toList()
+      ..sort((a, b) => a.sortOrder.compareTo(b.sortOrder));
     notifyListeners();
   }
 
   Future<void> addCategory(Category category) async {
-    await _repository.save(category);
-    _categories.add(category);
-    notifyListeners();
+    await _box.put(category.id, category);
+    await _loadCategories(); // reload from Hive
   }
 
   Future<void> updateCategory(Category category) async {
-    await _repository.save(category);
-
-    final index = _categories.indexWhere((c) => c.id == category.id);
-    _categories[index] = category;
-
-    notifyListeners();
+    await _box.put(category.id, category);
+    await _loadCategories();
   }
 
   Future<void> deleteCategory(String id) async {
-    await _repository.delete(id);
-    _categories.removeWhere((c) => c.id == id);
-    notifyListeners();
+    await _box.delete(id);
+    await _loadCategories();
   }
 
   Category? getCategoryById(String id) {
     try {
-      return _categories.firstWhere((p) => p.id == id);
+      return _categories.firstWhere((c) => c.id == id);
     } catch (_) {
       return null;
     }
   }
 
-  List<Category> get allCategories {
-  return _box.values.toList()
-    ..sort((a, b) => a.sortOrder.compareTo(b.sortOrder));
-  }
-  
   int get nextSortOrder {
     if (_box.isEmpty) return 0;
     return _box.values.map((c) => c.sortOrder).reduce((a, b) => a > b ? a : b) + 1;
   }
-
-
 }
